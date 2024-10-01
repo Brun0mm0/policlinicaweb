6fb51cfaa985a688374a6722a76eeb97e4c87b23
ProyectosOsssb <bprovenzano@osssb.com.ar>
2024-10-01 16:03:38 -0300

    en desarrollo popup con video de youtube + modal

--- assets/br-style/br-script.js
+++ assets/br-style/br-script.js
@@ -11,7 +11,7 @@ function isPopupExpired() {
     return timeDifference > EXPIRATION_TIME; // Verifica si ha pasado más de 1 hora
 }
 
-html =`
+const html =`
     <div class="br-popup-container fade-in">
         <div class="br-popup-cerrar">
             <div class="br-cerrar">
@@ -24,7 +24,11 @@ html =`
                 <span></span>
                 <span></span>
             </div>
-                <div id="video-popup"></div>
+                <div id="br-popup-modal-content"></div>
+                <div id="video-popup"></div> <!-- Aquí va el video -->
+                <div>
+                    <img src="/assets/images/coseguros-julio.png" alt="imagen-coseguros"/>
+                </div>
             <div class="br-popup-next">
                 <span></span>
                 <span></span>
@@ -32,61 +36,72 @@ html =`
         </div>
     </div>
     `
-                // <img src="assets/images/coseguros-julio.png" alt="">
-// if(isPopupExpired()) {
 
-    window.addEventListener('DOMContentLoaded', ()=> {
-        const template = document.createElement('template')
-        template.innerHTML = html
-        
-        const container = template.content.querySelector(".br-popup-container");
-        const btn = template.content.querySelector('.br-popup-cerrar')
-        
-        container.addEventListener('animationend', mostrar(0, container))
-            btn.addEventListener('click',()=> {
-                container.classList.add('fade-out')
-                container.addEventListener('animationend', ()=> {
-                    document.body.removeChild(container)
-            })
-        })
-        
-        document.body.appendChild(template.content)
-        const popupData = {
-            shown: true,
-            timestamp: new Date().getTime() // Guardamos la hora actual en milisegundos
-        };
-        // localStorage.setItem('popupShown', JSON.stringify(popupData));
-    })
-// }
+window.addEventListener('DOMContentLoaded', ()=> {
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    
+    const container = template.content.querySelector(".br-popup-container");
+    const btn = template.content.querySelector('.br-popup-cerrar');
+    
+    container.addEventListener('animationend', mostrar(0, container));
+    btn.addEventListener('click',()=> {
+        container.classList.add('fade-out');
+        container.addEventListener('animationend', ()=> {
+            document.body.removeChild(container);
+        });
+    });
+    
+    document.body.appendChild(template.content);
 
-        
+    // Aquí puedes asegurarte de que el div #video-popup está presente antes de cargar el reproductor
+    if (document.getElementById('video-popup')) {
+        createYouTubePlayer(); // Llama a la función para crear el reproductor de YouTube
+    }
+    
+    const popupData = {
+        shown: true,
+        timestamp: new Date().getTime() // Guardamos la hora actual en milisegundos
+    };
+    // localStorage.setItem('popupShown', JSON.stringify(popupData));
+});
+
 const mostrar = function (elemento, container) {
     const modal = Array.from(container.querySelectorAll("#modal-novedad"));
     
     if(elemento < modal.length) {
-        // console.log('Imagen', elemento)
         modal[elemento].classList.add('fade-in');
-
-        // setTimeout( function() {
-        //     modal[elemento].classList.add('fade-out');
-        //     mostrar((elemento + 1), container)
-        // },4500)
-    } 
-    // else {
-    //     container.classList.add('fade-out');
-    //     container.addEventListener('animationend', ()=> {
-    //         document.body.removeChild(container)
-    //     })
-    // }
+    }
 }
 
-let player
+// Función para crear el reproductor de YouTube
+function createYouTubePlayer() {
+    if (typeof YT !== 'undefined' && YT && YT.Player) { // Asegurarse de que la API de YouTube está cargada
+        player = new YT.Player('video-popup', {
+            height: '600',
+            width: '900',
+            videoId: 'csYJyuKetvg',
+            playerVars: {
+                'controls': 2,         // Mostrar solo los controles mínimos
+                'rel': 0,              // No mostrar videos relacionados de otros canales
+                'modestbranding': 1,   // Minimiza el branding de YouTube
+                'playsinline': 1,      // Reproducción en línea en dispositivos móviles
+                'autoplay': 1,         // Reproducción automática
+                'mute': 1,             // Silencia el video al iniciar
+                'fs': 1,               // Habilitar el botón de pantalla completa
+                'iv_load_policy': 3,   // Desactivar las anotaciones
+                'showinfo': 0          // Inicia en silencio para permitir autoplay
+              },
+        });
+    } else {
+        console.log('API de YouTube aún no está lista. Intentando nuevamente...');
+        setTimeout(createYouTubePlayer, 500); // Vuelve a intentar crear el reproductor después de un retraso
+    }
+}
 
+// Si la API de YouTube ya está lista, se invocará esta función
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('video-popup', {
-        height:'600',
-        width:'900',
-        videoId:'csYJyuKetvg'
-    })
+    if (document.getElementById('video-popup')) {
+        createYouTubePlayer(); // Llama a la función para crear el reproductor de YouTube
+    }
 }
-
